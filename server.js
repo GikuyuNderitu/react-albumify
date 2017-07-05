4bb1a8eccfb8f92c34415015bc99c786782e02c9
Gikuyu <gikuyunderitu@gmail.com>
2017-07-05 11:59:07 -0400

    Add a routing layer for authentication and users

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 "use strict";
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || '1337';
+
 
 
 const jwt = require('jsonwebtoken');
 
 
 require('./server/utils/mongoose');
+const authenticateRoutes = require('./server/controllers/auths').authenticate;
 
 require('./server/utils/middleware')(app);
 
+const users = require('./server/utils/routes').users
 const auth = require('./server/utils/routes').auth
 
-app.use('/users', auth);
+app.use('/users', users);
+app.use('/authenticate', auth)
 
-// If the user has a token, send it to them
-app.get("/", (req, res) => {
-	const token = req.cookies.token;
-	
-	if(token) {
-		
-	}
+console.log('error thrown?');
 
-
-})
-
-app.get('/*', (req, res) => {
+app.get('/*', authenticateRoutes, (req, res) => {
 	res.sendFile(path.resolve(__dirname, './client/dist/index.html'))
 })
 
